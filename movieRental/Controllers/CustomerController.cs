0ab368d1ea1b4c7a306cb6130626d66135bd0b52
Lonwabo Mnyaiza <lonwabo.mnyaiza@codeo.co.za>
2021-04-09 21:54:25 +0200

    :tada: initial commit.

---
+++ movieRental/Controllers/CustomerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using movieRental.Models;
+
+namespace movieRental.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customers = GetCustomers();
+            var filteredCustomer = customers.FirstOrDefault(c => c.Id == id);
+
+            if (filteredCustomer == null)
+                return HttpNotFound();
+
+            return View(filteredCustomer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            List<Customer> customers = new List<Customer>();
+            customers.Add(new Customer { Id = 1, Name = "Lonwabo Mnyaiza" });
+            customers.Add(new Customer { Id = 2, Name = "Mivuyo Mnyaiza" });
+
+            return customers;
+        }
+    }
+}
---
+++ movieRental/Controllers/MovieController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using movieRental.Models;
+using movieRental.ViewModels;
+
+namespace movieRental.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() { Name = "Matrix" };
+
+            var customers = new List<Customer>()
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().FirstOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+
+        //    if (String.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "name";
+
+        //    return Content(String.Format("pageIndex = {0}&sortBy={1}", pageIndex, sortBy));
+        //}
+
+        public ActionResult Edit(int id)
+        {
+            return Content(String.Format("Id = {0}", id));
+        }
+
+        [Route("movie/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year +"/"+ month);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            List<Movie> movies = new List<Movie>();
+            movies.Add(new Movie { Id = 1, Name = "Matrix" });
+            movies.Add(new Movie { Id = 2, Name = "Law Abiding Citizen" });
+
+            return movies;
+        }
+    }
+}
